07ea005fba908777f793b25df8ba36a52d79fab9
Donald J <84105603+catchmedonald@users.noreply.github.com>
2023-02-09 01:42:37 +0530

    Create SQL_Structure.sql

---
+++ SQL_Structure.sql
@@ -0,0 +1,14 @@
+#--ASSIGNMENT - 1
+
+--Create a database DEMO
+
+CREATE DATABASE DEMO;
+
+USE DEMO;
+
+
+#Create table EMP with the following fields:
+#EID NAME ADDR CITY DOB PHONE EMAIL id should be like 'EOOOI'
+#Insert 10 appropriate records in the Emp table
+#Use SELECT command to view the contents of emp table
+
